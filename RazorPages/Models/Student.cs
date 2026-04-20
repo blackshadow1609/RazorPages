5e8542fe394a0df349cd12cea5f9162b559c60bb
Blackshadow <blackshadow0916@yandex.ru>
2026-04-20 15:22:52 +0300

    RazorPages ContosoUniversity start

---
+++ RazorPages/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPages.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //Nav.properties
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
