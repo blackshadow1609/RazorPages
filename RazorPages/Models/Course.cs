816378b7af0a4e9ca121bad6bea9dac62921e783
Blackshadow <blackshadow0916@yandex.ru>
2026-05-06 16:06:54 +0300

    Student page DONE

--- RazorPages/Models/Course.cs
+++ RazorPages/Models/Course.cs
@@ -6,9 +6,9 @@ namespace RazorPages.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int CourseId { get; set; }
+        public int CourseID { get; set; }
         public string? Title { get; set; }
-        public string? Credits { get; set; }
+        public int Credits { get; set; }
 
         //Nav.prop.
         public ICollection<Enrollment>? Enrollments { get; set; }
