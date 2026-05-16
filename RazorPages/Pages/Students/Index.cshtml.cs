4cfedcbec0977c9d4b6b36874f6e1d1f2bd4beeb
Blackshadow <blackshadow0916@yandex.ru>
2026-05-16 04:12:00 +0300

    NameSort & DateSort DONE

--- RazorPages/Pages/Students/Index.cshtml.cs
+++ RazorPages/Pages/Students/Index.cshtml.cs
@@ -4,26 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using RazorPages.Data;
 using RazorPages.Models;
 
 namespace RazorPages.Pages.Students
 {
-    public class IndexModel : PageModel
-    {
-        private readonly RazorPages.Data.ContosoUniversityContext _context;
+	public class IndexModel : PageModel
+	{
+		private readonly RazorPages.Data.ContosoUniversityContext _context;
 
-        public IndexModel(RazorPages.Data.ContosoUniversityContext context)
-        {
-            _context = context;
-        }
+		public IndexModel(RazorPages.Data.ContosoUniversityContext context)
+		{
+			_context = context;
+		}
 
-        public IList<Student> Student { get;set; } = default!;
+		public string NameSort { get; set; }
+		public string DateSort { get; set; }
+		public string CurrentFilter { get; set; }
+		public string CurrentSort { get; set; }
+		public IList<Student> Students { get; set; } = default!;
 
-        public async Task OnGetAsync()
-        {
-            Student = await _context.Students.ToListAsync();
-        }
-    }
+		public async Task OnGetAsync(string sortOrder)
+		{
+			NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+			DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+			IQueryable<Student> students = from studedent in _context.Students select studedent;
+			switch (sortOrder)
+			{
+				case "name_desc": students = students.OrderByDescending(s => s.LastName); break;
+				case "date_desc": students = students.OrderByDescending(s => s.EnrollmentDate); break;
+				case "Date": students = students.OrderBy(s => s.EnrollmentDate); break;
+				default: students = students.OrderBy(s => s.LastName); break;
+			}
+
+			Students = await students.AsNoTracking().ToListAsync();
+			//Students = await _context.Students.ToListAsync();
+		}
+	}
 }
