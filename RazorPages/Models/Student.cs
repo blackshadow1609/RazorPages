6d1cef77c54aeefdb594520a7d3588318db079b5
Blackshadow <blackshadow0916@yandex.ru>
2026-05-31 13:06:03 +0300

    DataAnnotations in Student model DONE

--- RazorPages/Models/Student.cs
+++ RazorPages/Models/Student.cs
@@ -1,15 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RazorPages.Models
 {
     public class Student
     {
         public int ID { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
 
-        //Nav.prop.
-        public ICollection<Enrollment>? Enrollments { get; set; }
+		[Required]
+		[StringLength(50, ErrorMessage = "Фамилия может быть не более 50 символов")]
+		[DisplayName("Фамилия")]
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage ="Строка содержит недопустимые символы")]
+        public string LastName { get; set; }
+
+		[Required]
+		[StringLength(50, ErrorMessage = "Имя может быть не более 50 символов")]
+		[DisplayName("Имя")]
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Строка содержит недопустимые символы")]
+		public string FirstName { get; set; }
+		[DisplayName("Дата регистрации")]
+		[DataType(DataType.Date)]
+		public DateTime EnrollmentDate { get; set; }
+
+		[Display(Name = "Cтудент")]
+		public string FullName
+		{
+			get => $"{LastName} {FirstName}";
+		}
+
+		//Nav.prop.
+		public ICollection<Enrollment>? Enrollments { get; set; }
     }
 }
